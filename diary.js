e0024c9e264ce86f02860104a28bbe2cf1d53489
veritas373 <132289105+veritas373@users.noreply.github.com>
2025-07-24 15:14:52 +0200

    Create diary.js

---
+++ diary.js
@@ -0,0 +1,16 @@
+var diary_cjca = {
+  Week1:
+    {Day1: ["Today is the day when CJCA came out in HTB. \
+            I am super excited because this certification is \
+            exactly what I needed. I want to start preparing \
+            for the exam right now. This is my Day 1. #HTB #CJCA",
+            
+            "I enrolled in the HTB Certified Cybersecurity \
+            Associate's Job Role Path. I decided to add \
+            5 additional modules to my plans because they are \
+            included in the Information Security Foundations \
+            and SOC Analyst Prerequisites Skill Paths. \
+            I really want to become a full-rounded \
+            professional. #HTB #CJCA"]
+    }
+};
